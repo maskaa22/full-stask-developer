6e9ebb40ace3f5bb8823e64ae65410dda6fe7f30
maskaa22 <kyrosika565@gmail.com>
2024-01-28 20:28:54 +0200

    portfolio +1 --forest

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -31,16 +31,15 @@ function Portfolio() {
               <p>Mongo was used as a database.</p>
             </div>
           </section>
-          <section className='section'>
+          <section className='section' onClick={() => window.location.assign('https://forest-site.vercel.app/')}>
             <div className='image'>
-              <img alt='Olena Studio' src={'./img/olena_studio.png'} />
+              <img alt='Forest' src={'./img/forest.png'} />
             </div>
             <div className='text'>
-              <h3 className='h3'>Olena Studio</h3>
-              <p>Full Stack WebSite.</p>
-              <p>FrontEnd is written in the React and BackEnd in the Node.
-                Also, the site has an analysis for the administrator using React Chart.</p>
-              <p>The documentation is written by Swagger.</p>
+              <h3 className='h3'>Natural Forest</h3>
+              <p>FrontEnd - WebSite.</p>
+              <p>This site is written on React. The interesting thing about this project is the 3D main screen. 
+                By moving the mouse over the screen, you can view this structure more comprehensively.</p>
             </div>
           </section>
           <section className='section'>
